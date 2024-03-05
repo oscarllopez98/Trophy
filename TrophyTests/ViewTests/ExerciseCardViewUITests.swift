302cbab9df79cfe7f5226b29bb9735d46e387a05
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-04 19:54:29 -0800

    Added ExerciseCardViewUITests. 1 Fail, potentially because our target is TrophyTest instead of Trophy. Added accessibility identifiers to Button in ExerciseCardView and the body in ExerciseModalView

--- TrophyTests/ViewTests/ExerciseCardViewUITests.swift
+++ TrophyTests/ViewTests/ExerciseCardViewUITests.swift
@@ -6,28 +6,25 @@
 //
 
 import XCTest
+@testable import Trophy
 
 final class ExerciseCardViewUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testLaunchPerformance() throws {
@@ -38,4 +35,19 @@ final class ExerciseCardViewUITests: XCTestCase {
             }
         }
     }
+    
+    //MARK: Button Action Tests
+    func testButtonAction() throws {
+        let cardView: ExerciseCardView = ExerciseCardView(viewModel: ExerciseViewModel.sample())
+        //Tap the button
+        app.buttons[cardView.buttonAccessibilityIdentifier].tap()
+        
+        let modalView: ExerciseModalView = ExerciseModalView(viewModel: cardView.viewModel)
+        //Verify that the modal view is presented
+        XCTAssertTrue(app.otherElements[modalView.accessibilityIdentifier].exists)
+    }
+    
+    //MARK: Modal Presentation Tests
+    
+    
 }
