184a25e8c4df43ed9ad837a4fab82cea4425ed05
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-11 11:49:39 -0700

    Added and implemented LevelInputView. Added Hashable and CaseIterable to LevelAttribute.Level

---
+++ Trophy/Views/AttributeInputs/LevelInputView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelInputView.swift
+//  Trophy
+//
+//  Created by Oscar Lopez on 3/11/24.
+//
+
+import SwiftUI
+
+struct LevelInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelInputView()
+}
