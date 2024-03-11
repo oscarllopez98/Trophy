f0b1f79a16f45009647175e0589eabf504d14478
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-11 11:14:36 -0700

    Added and implemented basic RepsInputView

---
+++ Trophy/Views/AttributeInputs/RepsInputView.swift
@@ -0,0 +1,30 @@
+//
+//  RepsInputView.swift
+//  Trophy
+//
+//  Created by Oscar Lopez on 3/11/24.
+//
+
+import SwiftUI
+
+struct RepsInputView: View {
+    //State variables for tracking:
+    @State private var selectedReps: String = ""    //Reps value
+        
+    var body: some View {
+        
+        HStack {
+            TextField("Enter Reps", text: $selectedReps)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+                .padding()
+        }
+        .border(Color.black)
+        
+        Text("You Entered: \(selectedReps)")
+    }
+}
+
+#Preview {
+    RepsInputView()
+}
