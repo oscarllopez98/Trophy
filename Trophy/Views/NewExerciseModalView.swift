032fac97791b28e6f97a6b3617613481fb878b91
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-11 12:02:56 -0700

    Added all implementations of the AttributeInputs to the NewExerciseModalView. Need to make edits so user can input from NewExerciseModalView without closing the Modal

--- Trophy/Views/NewExerciseModalView.swift
+++ Trophy/Views/NewExerciseModalView.swift
@@ -10,62 +10,194 @@ import SwiftUI
 struct NewExerciseModalView: View {
     
     @ObservedObject var viewModel: ExerciseViewModel
-    @State private var isExpandedDuration = false
+    
+    //State values for Custom Pickers
+    @State private var isExpandedDistance = false
+    @State private var isExpandedTime = false
+    @State private var isExpandedSets = false
+    @State private var isExpandedReps = false
+    @State private var isExpandedWeight = false
+    @State private var isExpandedIntensity = false
+    @State private var isExpandedLevel = false
+
+    
 
     //Duration (hours, minutes, seconds)
     
     var body: some View {
         
-        
-        //Distance Attribute
-        
-        
-        //Time Attribute
-        
-        
-        //Sets Attribute
-        
-        
-        //Reps Attribute
-        
-        
-        //Weight Attribute
-        
-        
-        //Intensity Attribute
-        
-        
-        //Level Attribute
-        
-        
-        //Duration Attribute
-        Button(action : {
-            withAnimation {
-                self.isExpandedDuration.toggle()
-            }
-        }) {
-            VStack {
+        ScrollView {
+            //Distance Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedDistance.toggle()
+                }
+            }) {
+                VStack {
 
-                //Expand DurationPickerView
-                if isExpandedDuration {
-                    Text("- Duration")
-                        .font(.title)
-                        .foregroundStyle(.blue)
-                        .padding()
-                    DurationPickerView()
-                } else {
-                    Text("+ Duration")
-                        .font(.title)
-                        .foregroundStyle(.blue)
-                        .padding()
+                    //Expand DistancePickerView
+                    if isExpandedDistance {
+                        Text("- Distance")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        DistanceInputView()
+                    } else {
+                        Text("+ Distance")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
+                }
+            }
+            
+            //Time Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedTime.toggle()
+                }
+            }) {
+                VStack {
+
+                    //Expand SetsPickerView
+                    if isExpandedTime {
+                        Text("- Time")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        TimeInputView()
+                    } else {
+                        Text("+ Time")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
+                }
+            }
+            
+            //Sets Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedSets.toggle()
+                }
+            }) {
+                VStack {
+
+                    //Expand SetsPickerView
+                    if isExpandedSets {
+                        Text("- Sets")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        SetsInputView()
+                    } else {
+                        Text("+ Sets")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
+                }
+            }
+            
+            //Reps Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedReps.toggle()
+                }
+            }) {
+                VStack {
+
+                    //Expand RepsPickerView
+                    if isExpandedReps {
+                        Text("- Reps")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        RepsInputView()
+                    } else {
+                        Text("+ Reps")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
+                }
+            }
+            
+            
+            //Weight Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedWeight.toggle()
+                }
+            }) {
+                VStack {
+
+                    //Expand RepsPickerView
+                    if isExpandedWeight {
+                        Text("- Weight")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        WeightInputView()
+                    } else {
+                        Text("+ Weight")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
+                }
+            }
+            
+            
+            //Intensity Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedIntensity.toggle()
+                }
+            }) {
+                VStack {
+
+                    //Expand RepsPickerView
+                    if isExpandedIntensity {
+                        Text("- Intensity")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        IntensityInputView()
+                    } else {
+                        Text("+ Intensity")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
+                }
+            }
+            
+            
+            //Level Attribute
+            Button(action : {
+                withAnimation {
+                    self.isExpandedLevel.toggle()
+                }
+            }) {
+                VStack {
+
+                    //Expand LevelPickerView
+                    if isExpandedLevel {
+                        Text("- Level")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                        LevelInputView()
+                    } else {
+                        Text("+ Level")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                            .padding()
+                    }
                 }
             }
         }
-        
- 
-        
-        
-        
     }
 }
 
