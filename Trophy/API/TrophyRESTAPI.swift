ecb5079e74ec9bb26b7a74ae90374a5dbadcef45
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-22 16:18:13 -0700

    Added todo statements to TrophyRESTAPI about needing to implement API functions that we can use for testing

--- Trophy/API/TrophyRESTAPI.swift
+++ Trophy/API/TrophyRESTAPI.swift
@@ -13,15 +13,26 @@ class TrophyRESTAPI {
     let client = TROPHYTrophyRESTAPIClient.default()
     
     func testAPICall() {
-
         //Call the Get Exercises API
-        client.exercisesGet().continueWith { (task) -> Any? in
-            if let error = task.error {
-                print("Error: \(error)")
-            } else {
-                print("Reqeuest succeeded but no data was returned.")
-            }
+        //TODO: Implement API function that will return something that we can test
+        client.rootGet(op: "+", a: "1", b: "2").continueWith { (task) -> AnyObject? in
+            print(task)
+            self.showResult(task: task)
             return nil
+        }
+    }
+    
+    func showResult(task: AWSTask<AnyObject>) {
+        if let error = task.error {
+            print("Error: \(error)")
+        } else if let result = task.result {
+            if result is TROPHYResult {
+                let res = result as! TROPHYResult
+                print(String(format:"%@ %@ %@ = %@", res.input!.a!, res.input!.op!, res.input!.b!, res.output!.c!))
+            } else if result is NSDictionary {
+                let res = result as! NSDictionary
+                print("NSDictionary: \(res)")
+            }
         }
     }
     
