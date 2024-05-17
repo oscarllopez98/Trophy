7ce3a4618b27d79e75a16cd3ee3abd3bf5d95e02
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-05-17 10:36:18 -0700

    Refactored files due to error found where DistanceAttributeConverter was not detected by test target. Works now.

--- Trophy/API/Utils/Converters/DistanceAttributeConverter.swift
+++ Trophy/API/Utils/Converters/DistanceAttributeConverter.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+class DistanceAttributeConverter {
+    func convertToAPIFormat(_ distanceAttribute: DistanceAttribute) -> [String: Any] {
+        var attributeDict: [String: Any] = [:]
+        attributeDict["value"] = distanceAttribute.value
+        attributeDict["unit"] = distanceAttribute.unit?.symbolAsString
+        return attributeDict
+    }
+}
