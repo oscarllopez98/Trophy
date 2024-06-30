4627f58c5eeb287b153c8a7397d028900dab0bdc
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-06-30 13:23:31 -0700

    Added new component 'UserProfileInfoView' for viewing basic user info on the home page

---
+++ Trophy/Views/Components/UserProfileInfoView.swift
@@ -0,0 +1,55 @@
+//
+//  UserProfileInfoView.swift
+//  Trophy
+//
+//  Created by Oscar Lopez on 6/30/24.
+//
+
+import SwiftUI
+
+struct UserProfileInfoView: View {
+    
+    // System Names
+    let userProfileImageSystemName: String
+    let userLocationImageSystemName: String
+    
+    // Displays
+    let userProfileImage: Image
+    let userFullName: String
+    let userLocationImage: Image
+    let userLocation: String
+    
+    init() {
+        // Set: System Names
+        userProfileImageSystemName = "person.crop.circle"
+        userLocationImageSystemName = "mappin.circle"
+        
+        // Set: Displays
+        userProfileImage = Image(systemName: userProfileImageSystemName)
+        userLocationImage = Image(systemName: userLocationImageSystemName)
+        userFullName = "Firstname Lastname"
+        userLocation = "City, State"
+    }
+    var body: some View {
+        
+        // Home Image Dimensions
+        let userProfileImageWidth: CGFloat = 40
+        let userProfileImageHeight: CGFloat = 40
+        
+        VStack {
+            userProfileImage
+                .resizable()
+                .frame(width: userProfileImageWidth, height: userProfileImageHeight)
+            Text(userFullName)
+            HStack {
+                userLocationImage
+                Text(userLocation)
+            }
+        }
+        .border(Color.black)
+    }
+}
+
+#Preview {
+    UserProfileInfoView()
+}
