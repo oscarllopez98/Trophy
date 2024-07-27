0ca3ab65b93f52127e0e498befb0d7925d0453b2
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-07-26 23:39:02 -0700

    Updated ListView to include List and Delete on swipe.

--- Trophy/Views/Components/ExerciseCardListView.swift
+++ Trophy/Views/Components/ExerciseCardListView.swift
@@ -12,8 +12,7 @@ struct ExerciseCardListView: View {
     @ObservedObject var viewModel: ExerciseCardListViewModel
     
     var body: some View {
-        // Main Container
-        ScrollView {
+        VStack {
             if viewModel.isLoading {
                 VStack {
                     Spacer()
@@ -22,6 +21,7 @@ struct ExerciseCardListView: View {
                         .multilineTextAlignment(.center)
                     Spacer()
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             } else if viewModel.exercises.isEmpty {
                 VStack {
                     Text("You don't have any Exercises tracked yet!")
@@ -31,17 +31,39 @@ struct ExerciseCardListView: View {
                         .font(.subheadline)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
             } else {
-                ForEach(viewModel.exercises, id: \.id) { exercise in
-                    let exerciseCardViewModel = ExerciseCardViewModel(exercise: exercise)
-                    ExerciseCardView(viewModel: exerciseCardViewModel)
+                List {
+                    ForEach(viewModel.exercises, id: \.id) { exercise in
+                        let exerciseCardViewModel = ExerciseCardViewModel(exercise: exercise)
+                        ExerciseCardView(viewModel: exerciseCardViewModel)
+                            .swipeActions(edge: .trailing) {
+                                Button(action: {
+                                    // Handle delete action here
+                                    deleteExercise(exercise)
+                                }) {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                                .tint(.red)
+                            }
+                    }
                 }
+                .listStyle(PlainListStyle())  // Removes default list styling
+                .background(Color.clear)
+                .padding(0)  // Removes padding around the list
+                .frame(maxWidth: .infinity, maxHeight: .infinity)  // Ensures List takes full width and height
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+    
+    private func deleteExercise(_ exercise: Exercise) {
+        // Implement the deletion logic here
+        
+        // You may need to notify a parent view or perform additional actions
+        print("Deleting exercise: \(exercise.name)")
+    }
 }
-
 
 #Preview {
     ExerciseCardListView(viewModel: ExerciseCardListViewModel.sample())
